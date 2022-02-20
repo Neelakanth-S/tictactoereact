589f1ebf565b585927f17478af44a2c20ae9ca15
Neelakanth S <neelakanth2000@gmail.com>
2022-02-20 17:29:32 +0530

    1st commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react" // importing React library
+
+
+class Header extends React.Component {
+    render() {
+        return   <div>
+        <h2>Welcome to react</h2>
+        <p>fhfhfhfhfhfhfhfhfhfhfhffhfhfh</p>
+        <button>Explore</button>
+        
+                </div>
+
+
+    }
+}
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+
+function Main() {
+
+    return <div>
+        <ul>
+            <li> HTML</li>
+            <li> Css</li>
+        </ul>
+    </div>
+}
+export default Main;
